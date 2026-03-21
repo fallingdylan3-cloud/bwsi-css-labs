5393a2b4dee3bba85e876c0070b632fae8ba4854
fallingdylan3-cloud <fallingdylan3@gmail.com>
2026-03-21 16:36:02 -0400

    Added song lyrics
    from the song bibidy

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Dylan O" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Bidibobyboba bibibidibibidi oooooooo")
 if __name__ == "__main__":
     main()
